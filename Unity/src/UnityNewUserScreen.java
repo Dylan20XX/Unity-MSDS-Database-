3ecec771e3e4f0905a507594b7ac0bd709c8c5f6
348655606 <348655606@402X10662F0FFL2.EDU.YRDSB.NET>
2019-12-02 13:00:50 -0500

    CommentTest

--- Unity/src/UnityNewUserScreen.java
+++ Unity/src/UnityNewUserScreen.java
@@ -1,3 +1,14 @@
+/*
+ * Name: Momin Naeem
+ * Date: Dec 2, 2019
+ * Course: ICS4U1
+ * Topic: Unity Project
+ * Title: NewUserScreen
+ * Description: This class controls the new user screen, where a user who doesn't have a 
+ * 				"USERNAME" and "PASSWORD" will have to make valid credentials. This screen 
+ * 				write their new information in a file to save for next time the user opens 
+ * 				the program.
+ */
 
 public class UnityNewUserScreen {
 
