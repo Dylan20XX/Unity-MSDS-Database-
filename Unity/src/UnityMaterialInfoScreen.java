005f71adbf9e605f8d0b0dcd013970a16eb1f707
Nick <Nick@192.168.0.14>
2019-12-05 07:56:00 -0500

    fixes

--- Unity/src/UnityMaterialInfoScreen.java
+++ Unity/src/UnityMaterialInfoScreen.java
@@ -29,7 +29,7 @@ public class UnityMaterialInfoScreen extends JFrame {
 	//Constructor method
 	public UnityMaterialInfoScreen() {
 		panelSetup();
-		//scrollPaneSetup();
+		infoSetup();
 		frameSetup();
 	}
 
