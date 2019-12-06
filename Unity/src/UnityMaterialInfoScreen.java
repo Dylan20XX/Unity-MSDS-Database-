16e69ffc2e8d9df66daee9b0c191545f99f90994
Nick <Nick@192.168.0.14>
2019-12-05 20:46:15 -0500

    Material Info Screen

--- Unity/src/UnityMaterialInfoScreen.java
+++ Unity/src/UnityMaterialInfoScreen.java
@@ -66,6 +66,7 @@ public class UnityMaterialInfoScreen extends JFrame implements ActionListener {
 	//Constructor method
 	public UnityMaterialInfoScreen() {
 		panelSetup();
+		infoSetup();
 		frameSetup();
 	}
 	
@@ -105,44 +106,66 @@ public class UnityMaterialInfoScreen extends JFrame implements ActionListener {
 	//This method sets up the scroll pane used 
 	private void infoSetup() {
 		
-		titleLabel.setText(material.getName());
+		//titleLabel.setText(material.getName());
 		
 		//Brand
 		brandLabel.setBounds(400, 150, 400, 50);
-		brandTextArea.setText(material.getBrand());
+		materialPanel.add(brandLabel);
 		brandTextArea.setBounds(400, 200, 400, 50);
+		//brandTextArea.setText(material.getBrand());
+		materialPanel.add(brandTextArea);
 		
 		//Hyperlink
-		hyperlinkButton.setBounds(200, 450, 200, 100);
+		hyperlinkButton.setBounds(140, 300, 200, 100);
 		hyperlinkButton.addActionListener(this);
+		materialPanel.add(hyperlinkButton);
 		
 		//StorageArea
-		storageAreaLabel.setBounds(400, 150, 400, 50);
-//		private JTextArea storageAreaTextArea = new JTextArea();
-//		
-//		//Precautions
-//		private JLabel precautionsLabel = new JLabel("Precautions");
-//		private JTextArea precautionsTextArea = new JTextArea();
-//		
-//		//Toxic
-//		private JLabel toxicLabel = new JLabel("Toxic?");
-//		private JTextArea toxicTextArea = new JTextArea();
-//		
-//		//Stability and Reactivity
-//		private JLabel stabilityLabel = new JLabel("Stability & Reactivity");
-//		private JTextArea stabilityTextArea = new JTextArea();
-//		
-//		//First aid measures
-//		private JLabel firstAidLabel = new JLabel("First Aid Measures");
-//		private JTextArea firstAidTextArea = new JTextArea();
-//		
-//		//Dangers
-//		private JLabel dangersLabel = new JLabel("Dangers");
-//		private JTextArea dangersTextArea = new JTextArea();
+		storageAreaLabel.setBounds(400, 250, 400, 50);
+		materialPanel.add(storageAreaLabel);
+		storageAreaTextArea.setBounds(400, 300, 400, 50);
+		//storageAreaTextArea.setText(material.getStorageArea());
+		materialPanel.add(storageAreaTextArea);
+		
+		//Precautions
+		precautionsLabel.setBounds(400, 350, 400, 50);
+		materialPanel.add(precautionsLabel);
+		precautionsTextArea.setBounds(400, 400, 400, 50);
+		//precautionsTextArea.setText(material.getPrecautions());
+		materialPanel.add(precautionsTextArea);
+		
+		//Toxic
+		toxicLabel.setBounds(820, 150, 400, 50);
+		materialPanel.add(toxicLabel);
+		toxicTextArea.setBounds(820, 200, 400, 50);
+		//toxicTextArea.setText(material.getToxic());
+		materialPanel.add(toxicTextArea);
+		
+		//Stability and Reactivity
+		stabilityLabel.setBounds(820, 250, 400, 50);
+		materialPanel.add(stabilityLabel);
+		stabilityTextArea.setBounds(820, 300, 400, 50);
+		//stabilityTextArea.setText(material.getStability());
+		materialPanel.add(stabilityTextArea);
+		
+		//First aid measures
+		firstAidLabel.setBounds(820, 350, 400, 50);
+		materialPanel.add(firstAidLabel);
+		firstAidTextArea.setBounds(820, 400, 400, 50);
+		//firstAidTextArea.setText(material.getFirstAid());
+		materialPanel.add(firstAidTextArea);
+		
+		//Dangers
+		dangersLabel.setBounds(820, 450, 400, 50);
+		materialPanel.add(dangersLabel);
+		dangersTextArea.setBounds(820, 500, 400, 50);
+		//dangersTextArea.setText(material.getDangers());
+		materialPanel.add(dangersTextArea);
 		
 		//Link to MSDS
-		msdsButton.setBounds(200, 550, 200, 100);
+		msdsButton.setBounds(140, 450, 200, 100);
 		msdsButton.addActionListener(this);
+		materialPanel.add(msdsButton);
 		
 	}
 	
