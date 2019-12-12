ce70eb1d6fd75008231b6d1cf5f5fb0df1a343e3
072921778 <072921778@402X10663F0FFL2.EDU.YRDSB.NET>
2019-12-12 13:21:27 -0500

    asset

--- Unity/src/Assets.java
+++ Unity/src/Assets.java
@@ -8,14 +8,14 @@ public class Assets {
 	public static ImageIcon loginBackground = backgroundImageSetup("Images/GUI/Login screen.png");
 	
 	//Buttons 
-	public static ImageIcon createButton = buttonImageSetup("Images/GUI/Create Project Button.png");
-	public static ImageIcon deleteButton = buttonImageSetup("Images/GUI/Delete Button.png");
+	//public static ImageIcon createButton = buttonImageSetup("Images/GUI/Create Project Button.png");
+	//public static ImageIcon deleteButton = buttonImageSetup("Images/GUI/Delete Button.png");
 	
 	//Material Images
-	public static ImageIcon charcoalPencil = buttonImageSetup("Images/Materials/Charcoal pencil.jpg");
-	public static ImageIcon instantPaperMache= buttonImageSetup("Images/Materials/Instant Paper Mache.jpg");
-	public static ImageIcon oilPastel = buttonImageSetup("Images/Materials/Oil Pastel.jpg");
-	public static ImageIcon pencilColourSticks = materialImageSetup("Images/Materials/Pencils _ Colour Sticks.jpg");	
+	//public static ImageIcon charcoalPencil = buttonImageSetup("Images/Materials/Charcoal pencil.jpg");
+	//public static ImageIcon instantPaperMache= buttonImageSetup("Images/Materials/Instant Paper Mache.jpg");
+	//public static ImageIcon oilPastel = buttonImageSetup("Images/Materials/Oil Pastel.jpg");
+	//public static ImageIcon pencilColourSticks = materialImageSetup("Images/Materials/Pencils _ Colour Sticks.jpg");	
 	
 	//This method returns a scaled image (200x100)
 	private static ImageIcon buttonImageSetup(String imagePath) {
@@ -32,7 +32,7 @@ public class Assets {
 	//This method returns a scaled image 1280x720)	
 	private static ImageIcon backgroundImageSetup(String imagePath) {
 		return new ImageIcon(new ImageIcon(imagePath).
-				getImage().getScaledInstance(1280, 720, Image.SCALE_SMOOTH));
+				getImage().getScaledInstance(1280, 720, Image.SCALE_FAST));
 	}
 	
 }
