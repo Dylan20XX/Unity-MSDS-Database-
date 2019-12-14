3f2998623c79de5c36da03d63ad312a31756b05b
Kavish0902 <58439928+Kavish0902@users.noreply.github.com>
2019-12-14 16:58:24 -0500

    Update UnityReportScreen.java

--- Unity/src/UnityReportScreen.java
+++ Unity/src/UnityReportScreen.java
@@ -1,407 +1,87 @@
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.awt.event.*;
+import javax.swing.*;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+public class UnityTextInputScreen extends JFrame implements ActionListener{
 /**
- * 
- * @author Humza Belmo
- *
+ * This class was a prototype and didn't end up being used 
+ * @author Kavish
  */
-public class UnityReportScreen extends JFrame implements ActionListener {\
-	/**
-	 * this class controls the Report Screen GUI
-	 * @param null
-	 */
+	JPanel textinputpanel = new JPanel();
+	JMenuBar menu = new JMenuBar();
+	JMenu file = new JMenu("file");
+	JMenuItem SAVE = new JMenuItem("SAVE");
+	JMenuItem BACK = new JMenuItem("BACK");
+	JLabel title = new JLabel("Text Input");
+	JTextArea main = new JTextArea();
+	JTextArea links = new JTextArea();
+	JButton linktext = new JButton("Link Text");
+	JButton deletelink = new JButton("Delete Link");
+	JLabel maintextlabel = new JLabel("Art Information");
+	JLabel hyperlinks = new JLabel("Current Link");
 	
-	/**
-	 * Main Method
-	 * @param args
-	 */
-	public static void main (String[] args) {
-		FileInput.readMaterials();
-		new UnityReportScreen();
-	}
+	public UnityTextInputScreen(){
+		PanelSetup();
+		frameSetup(); 
+}
 	
-	private String projectName;
-	private String projectDescription;
-	private String projectSources;
-	
-	private JMenuBar menubar = new JMenuBar();
-	
-	private JLabel titleLabel = new JLabel("UNITY");
-	
-	private JLabel projectNameLabel = new JLabel("Project Name");
-	private JLabel userInputLabel = new JLabel("Project Description");
-	private JLabel sourcesLabel = new JLabel("Information Sources");	
-	
-	private JTextField projectNameTextArea = new JTextField();
-	private JTextArea descriptionTextArea = new JTextArea();
-	private JTextArea sourcesTextArea = new JTextArea();	
-	private JScrollPane descriptionScrollPane;
-	private JScrollPane sourcesScrollPane;
-	
-	private JPanel materialPanel = new JPanel();
-	private JScrollPane materialScrollPane;
-	
-	//Panels within material panel
-	private JPanel quantityButtonPanel = new JPanel();
-	private JPanel materialQuantityPanel = new JPanel();
-	private JPanel infoButtonPanel = new JPanel();
-	private JPanel materialNamePanel = new JPanel();
-	
-	private JButton saveButton = new JButton("Save Project");
-	private JButton exitButton = new JButton("<html><center>Exit Without<br>Saving</center></html>");
-	private JButton materialSelectButton = new JButton("<html><center>Return to<br>Material Selection</center></html>");
-	
-	/**
-	 * This is the constructor method
-	 * @param null
-	 */
-	public UnityReportScreen() {
-		menuBarSetup();
-		labelSetup();
-		textAreaSetup();
-		materialPanelSetup();
-		addMaterialButtons();
-		buttonSetup();
-		frameSetup();
-	}
-	
-	/**
-	 * This method sets up the menu bar
-	 * @param null
-	 */
-	private void menuBarSetup() {
 		
-        //construct preComponents
-        JMenu fileMenu = new JMenu ("File");
-        JMenuItem file_option_1Item = new JMenuItem ("File Option 1");
-        fileMenu.add (file_option_1Item);
-        JMenuItem file_option_2Item = new JMenuItem ("File Option 2");
-        fileMenu.add (file_option_2Item);
-        JMenu editMenu = new JMenu ("Edit");
-        JMenuItem project_1Item = new JMenuItem ("Project 1");
-        editMenu.add (project_1Item);
-        JMenuItem project_2Item = new JMenuItem ("Project 2");
-        editMenu.add (project_2Item);
-        JMenu exitMenu = new JMenu ("Exit");
-        JMenuItem exit_programItem = new JMenuItem ("Exit Program");
-        exitMenu.add (exit_programItem);
-        JMenuItem exit___saveItem = new JMenuItem ("Exit & Save");
-        exitMenu.add (exit___saveItem);
-        
-        menubar.add (fileMenu);
-        menubar.add (editMenu);
-        menubar.add (exitMenu);
-        
-        menubar.setBounds (0, 0, 1280, 25);
-        
-        add(menubar);
-	}
-	/**
-	 * This method sets up the JLabels
-	 * @param null
-	 */
-	private void labelSetup() {
-        titleLabel.setFont(new Font( "Serif", Font.PLAIN, 36));
-		titleLabel.setForeground(Color.BLACK);
-		titleLabel.setBounds (600, 15, 480, 80);
-		add(titleLabel);
-	}
-	/**
-	 * This method sets up the TextAreas
-	 * @param null
-	 */
-	private void textAreaSetup() {
-		
-		projectNameLabel.setBounds (35, 100, 400, 20);
-		userInputLabel.setBounds (35, 150, 400, 20);
-		sourcesLabel.setBounds (35, 530, 400, 20);
-		
-		add(projectNameLabel);
-		add(userInputLabel);
-		add(sourcesLabel);
-		
-		projectNameTextArea.setBounds (35, 120, 400, 20);
-		descriptionTextArea.setLineWrap(true);
-		descriptionTextArea.setWrapStyleWord(true);
-		sourcesTextArea.setLineWrap(true);
-		sourcesTextArea.setWrapStyleWord(true);
-		
-		descriptionScrollPane = new JScrollPane(descriptionTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		descriptionScrollPane.setBounds (35, 170, 400, 350);
-		
-		sourcesScrollPane = new JScrollPane(sourcesTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		sourcesScrollPane.setBounds (35, 550, 400, 105);
-		
-		projectNameTextArea.setText(Database.currentProject.getName());
-		descriptionTextArea.setText(Database.currentProject.getProjectDescription());
-		sourcesTextArea.setText(Database.currentProject.getSources());
-		
-		add(projectNameTextArea);
-		add(descriptionScrollPane);
-		add(sourcesScrollPane);
-		
-	}
-	/**
-	 * This method sets up the Material Panel
-	 * @param null
-	 */
-	private void materialPanelSetup() {
-		
-		materialPanel.setLayout(new BoxLayout(materialPanel, BoxLayout.X_AXIS));
-		materialPanel.setBackground(Color.DARK_GRAY);
-		
-		//Panels within the material panel
-		quantityButtonPanel.setLayout(new BoxLayout(quantityButtonPanel, BoxLayout.Y_AXIS));
-		quantityButtonPanel.setBackground(Color.DARK_GRAY);
-		materialPanel.add(quantityButtonPanel);
-		materialQuantityPanel.setLayout(new BoxLayout(materialQuantityPanel, BoxLayout.Y_AXIS));
-		materialQuantityPanel.setBackground(Color.LIGHT_GRAY);
-		materialPanel.add(materialQuantityPanel);
-		infoButtonPanel.setLayout(new BoxLayout(infoButtonPanel, BoxLayout.Y_AXIS));
-		infoButtonPanel.setBackground(Color.DARK_GRAY);
-		materialPanel.add(infoButtonPanel);
-		materialNamePanel.setLayout(new BoxLayout(materialNamePanel, BoxLayout.Y_AXIS));
-		materialNamePanel.setBackground(Color.WHITE);
-		materialPanel.add(materialNamePanel);
-		
-		//Add the scroll pane 
-		materialScrollPane = new JScrollPane(materialPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		materialScrollPane.setBounds(450,120,800,400);
-		add(materialScrollPane);
-		
-	}
-	
-	//This method sets up the save and back buttons
-	/**
-	 * This method sets up the JButtons
-	 * @param null
-	 */
-	private void buttonSetup() {
-		
-		materialSelectButton.addActionListener(this);
-		materialSelectButton.setFont(new Font( "Arial", Font.PLAIN, 24));
-		materialSelectButton.setBounds (450, 550, 200, 105);
-		add(materialSelectButton);
-		
-		exitButton.addActionListener(this);
-		exitButton.setFont(new Font( "Arial", Font.PLAIN, 24));
-		exitButton.setBounds (670, 550, 200, 105);
-		add(exitButton);
-		
-		saveButton.addActionListener(this);
-		saveButton.setFont(new Font( "Arial", Font.PLAIN, 24));
-		saveButton.setBounds (950, 550, 300, 105);
-		add(saveButton);
-		
-	}
-	
-	//This method sets up the frame
-	/**
-	 * This method sets up the JFrame
-	 * @param null
-	 */
 	private void frameSetup() {
-		
-		//Set the title and frame size
-		setTitle("Unity");
-		setSize(1280, 720);
+		// TODO Auto-generated method stub
+		setSize(1280,720);
 		setLayout(null);
-		//getContentPane().setBackground(new Color(191,231,247)); //Blue
-		//getContentPane().setBackground(new Color(82,66,209)); //Dark purple
-		getContentPane().setBackground(new Color(140,100,209)); //Light purple
-		
-		//Prevent the program from running when the frame is closed, prevent the 
-		//frame from being resized, and make the frame visible
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setResizable(false);
+		add(textinputpanel);
 		setVisible(true);
-		
+		getContentPane().setBackground(Color.white);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Text Input");
+		setJMenuBar(menu);
 	}
-	/**
-	 * This method adds new material buttons
-	 * @param null
-	 */
-	private void addMaterialButtons() {
+
+
+	private void PanelSetup() {
+		// TODO Auto-generated method stub
+		textinputpanel.setBounds(0, 0, 1270, 710);
+		textinputpanel.setLayout(null);
+		textinputpanel.setBackground(Color.CYAN);
+		file.add(SAVE);
+		file.add(BACK);
+		menu.add(file);
 		
-		quantityButtonPanel.removeAll();
-		infoButtonPanel.removeAll();
-		materialNamePanel.removeAll();
-		materialQuantityPanel.removeAll();
+		title.setBounds(530, 0, 900, 100);
+		title.setFont(new Font("Serif", Font.PLAIN, 64));
+		textinputpanel.add(title);
 		
-		removeActionListeners();
+		maintextlabel.setBounds(350, 80, 200, 50);
+		maintextlabel.setFont(new Font("Serif", Font.PLAIN, 24));
+		textinputpanel.add(maintextlabel);
 		
-		for(Material currentMaterial: Database.currentProject.getMaterialList()) {
-			
-			currentMaterial.setupComponents();
-			
-			currentMaterial.getQuantityButton().addActionListener(this);
-			quantityButtonPanel.add(currentMaterial.getQuantityButton());
-			
-			currentMaterial.getInfoButton().addActionListener(this);
-			infoButtonPanel.add(currentMaterial.getInfoButton());
-			
-			materialNamePanel.add(currentMaterial.getNameLabel());
-			
-			materialQuantityPanel.add(currentMaterial.getQuantityField());
-			
-		}
+		hyperlinks.setBounds(350, 335, 200, 50);
+		hyperlinks.setFont(new Font("Serif", Font.PLAIN, 24));
+		textinputpanel.add(hyperlinks);
 		
-		repaint();
-		revalidate();
+		main.setBounds(350, 125, 600, 150);
+		textinputpanel.add(main);
 		
-//		for(Material currentMaterial: Database.materials) {
-//			
-//			currentMaterial.setupComponents();
-//			
-//			currentMaterial.getQuantityButton().addActionListener(this);
-//			quantityButtonPanel.add(currentMaterial.getQuantityButton());
-//			
-//			currentMaterial.getInfoButton().addActionListener(this);
-//			infoButtonPanel.add(currentMaterial.getInfoButton());
-//			
-//			materialNamePanel.add(currentMaterial.getNameLabel());
-//			
-//			materialQuantityPanel.add(currentMaterial.getQuantityField());
-//			
-//		}
+		links.setBounds(350, 375, 600, 30);
+		textinputpanel.add(links);
 		
+		linktext.setBounds(235,475,200,100);
+		linktext.addActionListener(this);
+		textinputpanel.add(linktext); 
+
+		deletelink.setBounds(857,475,200,100);
+		deletelink.addActionListener(this);
+		textinputpanel.add(deletelink);
 	}
+
+
 
 	@Override
-	/**
-	 * This method checks for user inputs in order to perform tasks
-	 * @param userinput
-	 */
 	public void actionPerformed(ActionEvent e) {
-		
-		for(Material currentMaterial: Database.currentProject.getMaterialList()) {
-			
-			if(e.getSource() == currentMaterial.getQuantityButton()) {
-				currentMaterial.setQuantity(Integer.parseInt(currentMaterial.getQuantityField().getText()));
-				return;
-			} else if (e.getSource() == currentMaterial.getInfoButton()) {
-				removeActionListeners();
-				new UnityMaterialInfoScreen(currentMaterial, 1);
-				this.dispose();
-				return;
-			}
-			
-		}
-		
-		if(e.getSource() == saveButton) {
-			if(!checkValidProjectName())
-				return;
-			saveProject();
-			new UnityProjectListScreen();
-			this.dispose();
-		} else if(e.getSource() == materialSelectButton) {
-			Database.currentProject.setName(projectNameTextArea.getText());
-			Database.currentProject.setProjectDescription(descriptionTextArea.getText());
-			Database.currentProject.setSources(sourcesTextArea.getText());
-			new UnityMaterialSelectionScreen();
-			this.dispose();
-		} else if (e.getSource() == exitButton) {
-			new UnityProjectListScreen();
-			this.dispose();
-		}
+		// TODO Auto-generated method stub
 		
 	}
-	
-	//This method removes the action listers from material buttons
-	/**
-	 * This method removes action listeners from material buttons
-	 * @param null
-	 */
-	private void removeActionListeners() {
-		
-//		for(Material currentMaterial: Database.currentProjectMaterials) {
-//			currentMaterial.getQuantityButton().removeActionListener(this);
-//			currentMaterial.getInfoButton().removeActionListener(this);
-//		}
-		
-		for(Material currentMaterial: Database.materials) {
-			currentMaterial.getQuantityButton().removeActionListener(this);
-			currentMaterial.getInfoButton().removeActionListener(this);
-		}
-		
-	}
-	/**
-	 * This method Checks if the user uses a valid project name
-	 * @param null
-	 */
-	private boolean checkValidProjectName() {
-		//A filename can't contain any of the following characters
-		//\/:*?"<>|
-		if(projectNameTextArea.getText().contains("\\") || projectNameTextArea.getText().contains("/") || 
-				projectNameTextArea.getText().contains(":") || projectNameTextArea.getText().contains("*") || 
-				projectNameTextArea.getText().contains("?") || projectNameTextArea.getText().contains("\"") || 
-				projectNameTextArea.getText().contains("<") || projectNameTextArea.getText().contains(">") || 
-				projectNameTextArea.getText().contains("|") || projectNameTextArea.getText().isEmpty() || projectNameTextArea.getText().matches("[ ]+")) {
-			return false;
-		} else {
-			projectName = projectNameTextArea.getText();
-			projectDescription = descriptionTextArea.getText();
-			projectSources = sourcesTextArea.getText();
-			return true;
-		}
-		
-	}
-	/**
-	 * This method saves users projects to our database
-	 * @param null
-	 */
-	private void saveProject() {
-		
-		String file = String.format("Users/%s/%s", Database.currentUser, projectName);
-		File filepath = new File(file);
 
-		//if(!filepath.exists() && !filepath.isDirectory()) {
-		if(!filepath.isDirectory()) {
-			
-			try {
-
-				//Write data to a file
-				PrintWriter pr = new PrintWriter(file);
-				
-				pr.println(projectName);
-				
-				//Replace all carriage returns with @
-				pr.print(projectDescription.replace("\n", "@"));
-				pr.println();
-				
-				pr.print(projectSources.replace("\n", "@"));
-				pr.println();
-				
-				pr.println(Database.currentProject.getMaterialList().size());
-				for(Material currentMaterial: Database.currentProject.getMaterialList()) {
-					pr.println(currentMaterial.getName() + "\n" + currentMaterial.getQuantity());
-				}
-				
-				pr.close();
-
-			} catch (FileNotFoundException e) {
-				System.out.println("Save Failed");
-			}
-
-		}
-		
-	}
-	
 }
