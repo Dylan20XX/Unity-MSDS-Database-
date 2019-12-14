e39189492fb5a41431d11dda4e5990cdc49066f4
Kavish0902 <58439928+Kavish0902@users.noreply.github.com>
2019-12-14 16:52:09 -0500

    Update Assets.java

--- Unity/src/Assets.java
+++ Unity/src/Assets.java
@@ -1,9 +1,17 @@
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
-
+/**
+ * 
+ * @author Dylan
+ *
+ */
 public class Assets {
-	
+	/**
+	 * This class is used for creating scaled images instead of having to import everytime.
+	 * @param imageicon
+	 * @see UnityProjectListScreen
+	 */
 	//Backgrounds
 	public static ImageIcon loginBackground = backgroundImageSetup("Images/GUI/Login screen.png");
 	
@@ -19,17 +27,27 @@ public class Assets {
 	//public static ImageIcon powderedMaterials = materialImageSetup("Images/Materials/Powdered Materials.jpg");	
 	
 	//This method returns a scaled image (200x100)
+	/**
+	 * @param imagePath
+	 * @return ScaledImage
+	 */
 	private static ImageIcon buttonImageSetup(String imagePath) {
 		return new ImageIcon(new ImageIcon(imagePath).
 				getImage().getScaledInstance(200, 100, Image.SCALE_SMOOTH));
 	}
-	
+	/**
+	 * @param imagePath
+	 * @return ScaledImage
+	 */
 	//This method returns a scaled image (200x200)
 	private static ImageIcon materialImageSetup(String imagePath) {
 		return new ImageIcon(new ImageIcon(imagePath).
 				getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH));
 	}
-	
+	/**
+	 * @param imagePath
+	 * @return ScaledImage
+	 */
 	//This method returns a scaled image 1280x720)	
 	private static ImageIcon backgroundImageSetup(String imagePath) {
 		return new ImageIcon(new ImageIcon(imagePath).
