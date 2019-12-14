a63b8a0e63b74d30edaa9be02e724b0c22165768
Kavish0902 <58439928+Kavish0902@users.noreply.github.com>
2019-12-14 16:54:07 -0500

    Update Project.java

--- Unity/src/Project.java
+++ Unity/src/Project.java
@@ -2,9 +2,21 @@ import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
-
+/** * @author Kavish Urethireswaran
+ * Date: December 15, 2019
+ * Course: ICS4U1
+ * Topic: Unity Project
+ * Title: Project Object
+ * Description: This class is primarily used to create project objects which can be manipulated
+ * 				by the user. Each project has a name, material list, a list of sources, and a 
+ * 				project description. This will then be used by the user when navigating through the 
+ * 				Project List screen.
+ */
 public class Project {
-	
+	/**
+	 * This class is used to create project objects which can then be edited or deleted.
+	 * @param null;
+	 */
 	private String name;
 	private String projectDescription;
 	private String sources;
@@ -15,10 +27,18 @@ public class Project {
 	private JLabel nameLabel = new JLabel();
 	
 	//Constructor methods
+	/**
+	 * This is the constructor method
+	 * @param null
+	 */
 	public Project() {
 	}
 
 	//Getters and setters
+	/**
+	 * This is the constructor method
+	 * @param null
+	 */
 	public Project(String name) {
 		this.name = name;
 		componentSetup();
@@ -27,7 +47,10 @@ public class Project {
 	public String getName() {
 		return name;
 	}
-
+	/**
+	 * 
+	 * @return name;
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -35,7 +58,10 @@ public class Project {
 	public String getProjectDescription() {
 		return projectDescription;
 	}
-
+	/**
+	 * 
+	 * @return projectDescription;
+	 */
 	public void setProjectDescription(String projectDescription) {
 		this.projectDescription = projectDescription;
 	}
@@ -43,7 +69,10 @@ public class Project {
 	public String getSources() {
 		return sources;
 	}
-
+	/**
+	 * 
+	 * @return sources;
+	 */
 	public void setSources(String sources) {
 		this.sources = sources;
 	}
@@ -51,7 +80,10 @@ public class Project {
 	public ArrayList<Material> getMaterialList() {
 		return materialList;
 	}
-
+	/**
+	 * 
+	 * @return materialList;
+	 */
 	public void setMaterialList(ArrayList<Material> materialList) {
 		this.materialList = materialList;
 	}
@@ -59,7 +91,10 @@ public class Project {
 	public JButton getSelectButton() {
 		return selectButton;
 	}
-
+	/**
+	 * 
+	 * @return selectButton;
+	 */
 	public void setSelectButton(JButton selectButton) {
 		this.selectButton = selectButton;
 	}
@@ -67,12 +102,17 @@ public class Project {
 	public JLabel getNameLabel() {
 		return nameLabel;
 	}
-
+	/**
+	 * 
+	 * @return nameLabel;
+	 */
 	public void setNameLabel(JLabel nameLabel) {
 		this.nameLabel = nameLabel;
 	}
 
-	//This method sets up the components to be used in the 
+	/**
+	 * This method sets up the components to be used
+	 */
 	public void componentSetup() {
 		
 		//Set up the button
@@ -91,7 +131,11 @@ public class Project {
 		
 	}
 	
-	//This method adds a specific quantity of a material to a project
+	/**
+	 * This method adds a specific quantity of a material to a project
+	 * @param material
+	 * @param count
+	 */
 	public void addMaterial(Material material, int count) {
 		
 		boolean containsMaterial = false;
