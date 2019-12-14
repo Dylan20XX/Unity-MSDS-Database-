c7c5ee27d42949a7314b773295894b66a04594ef
Kavish0902 <58439928+Kavish0902@users.noreply.github.com>
2019-12-14 16:58:43 -0500

    Update User.java

--- Unity/src/User.java
+++ Unity/src/User.java
@@ -1,21 +1,41 @@
 import javax.swing.JTextArea;
-
+/**
+ * 
+ * @author everyone
+ *
+ */
 public class User {
-	
+	/**
+	 * This class creates new user objects
+	 * @param user, pass
+	 */
 	private String user;
 	private String pass;
 	
+	/**
+	 * 
+	 * This is the constructor method
+	 * @param user
+	 * @param pass
+	 */
 	public User(String user, String pass) {
 		
 		this.user = user;
 		this.pass = pass;
 		
 	}
-
+	/**
+	 * This is the constructor method
+	 * @param null
+	 */
 	public User() {
 	}
 	
 	@Override
+	/**
+	 * This method creates a toString
+	 * @param null
+	 */
 	public String toString() {
 		return "User [user=" + user + ", pass=" + pass + "]";
 	}
@@ -23,7 +43,10 @@ public class User {
 	public String getUser() {
 		return user;
 	}
-
+	/**
+	 * This method creates a username
+	 * @param null
+	 */
 	public void setUser(String user) {
 		this.user = user;
 	}
@@ -31,7 +54,10 @@ public class User {
 	public String getPass() {
 		return pass;
 	}
-
+	/**
+	 * This method creates a password
+	 * @param null
+	 */
 	public void setPass(String pass) {
 		this.pass = pass;
 	}
